61a4f53f326bf66f25671cd66366a5c5163a4366
maksim.leskin <maksim.leskin@methed.ru>
2022-09-02 15:36:53 +0300

    fix

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // импорт стандартных библиотек Node.js
-const {readFileSync} = require('fs');
-const {createServer} = require('http');
-
+const { readFileSync } = require("fs");
+const { createServer } = require("http");
 
 // файл для базы данных
-const DB_FILE = process.env.DB_FILE || './db.json';
+const DB_FILE = process.env.DB_FILE || "./db.json";
 // номер порта, на котором будет запущен сервер
 const PORT = process.env.PORT || 3000;
 // префикс URI для всех методов приложения
-const URI_PREFIX = '/api/goods';
+const URI_PREFIX = "/api/goods";
 
 /**
  * Класс ошибки, используется для отправки ответа с определённым кодом и описанием ошибки
@@ -38,14 +37,13 @@ function shuffle(startArray) {
  */
 
 function randomGoods(goods) {
-  const discountGoods = goods.filter(item => item.discountPrice);
+  const discountGoods = goods.filter((item) => item.discountPrice);
   const discountGoodsRandom = shuffle(discountGoods);
   if (discountGoodsRandom.length > 16) {
     discountGoodsRandom.length = 16;
   }
   return discountGoodsRandom;
 }
-
 
 /**
  * Возвращает список товаров из базы данных
@@ -54,27 +52,26 @@ function randomGoods(goods) {
  * category: string, image: string}[]} Массив товаров
  */
 function getGoodsList(params = {}) {
-  console.log(params)
-  const goods = JSON.parse(readFileSync(DB_FILE) || '[]');
+  console.log(params);
+  const goods = JSON.parse(readFileSync(DB_FILE) || "[]");
   if (params.search) {
     const search = params.search.trim().toLowerCase();
-    return goods.filter(item => item.title.toLowerCase().includes(search));
+    return goods.filter((item) => item.title.toLowerCase().includes(search));
   }
 
   if (params.category) {
     const category = params.category.trim().toLowerCase();
     const regExp = new RegExp(`^${category}$`);
-    return goods.filter(item => regExp.test(item.category.toLowerCase()));
+    return goods.filter((item) => regExp.test(item.category.toLowerCase()));
   }
 
   if (params.list) {
     const list = params.list.trim().toLowerCase();
-    return goods.filter(item => list.includes(item.id));
+    return goods.filter((item) => list.includes(item.id));
   }
 
   return randomGoods(goods);
 }
-
 
 /**
  * Возвращает объект товара по его ID
@@ -84,29 +81,28 @@ function getGoodsList(params = {}) {
  * category: string, image: string}} Объект клиента
  */
 function getItems(itemId) {
-  const goods = JSON.parse(readFileSync(DB_FILE) || '[]');
-  const item = goods.find(({id}) => id === itemId);
-  console.log(itemId)
-  if (!item) throw new ApiError(404, {message: 'Item Not Found'});
+  const goods = JSON.parse(readFileSync(DB_FILE) || "[]");
+  const item = goods.find(({ id }) => id === itemId);
+  console.log(itemId);
+  if (!item) throw new ApiError(404, { message: "Item Not Found" });
   return item;
 }
-
 
 // создаём HTTP сервер, переданная функция будет реагировать на все запросы к нему
 module.exports = server = createServer(async (req, res) => {
   // req - объект с информацией о запросе, res - объект для управления отправляемым ответом
 
   // этот заголовок ответа указывает, что тело ответа будет в JSON формате
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader("Content-Type", "application/json");
 
   // CORS заголовки ответа для поддержки кросс-доменных запросов из браузера
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   // запрос с методом OPTIONS может отправлять браузер автоматически для проверки CORS заголовков
   // в этом случае достаточно ответить с пустым телом и этими заголовками
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     // end = закончить формировать ответ и отправить его клиенту
     res.end();
     return;
@@ -115,34 +111,33 @@ module.exports = server = createServer(async (req, res) => {
   // если URI не начинается с нужного префикса - можем сразу отдать 404
   if (!req.url || !req.url.startsWith(URI_PREFIX)) {
     res.statusCode = 404;
-    res.end(JSON.stringify({message: 'Not Found'}));
+    res.end(JSON.stringify({ message: "Not Found" }));
     return;
   }
 
   // убираем из запроса префикс URI, разбиваем его на путь и параметры
-  const [uri, query] = req.url.substr(URI_PREFIX.length).split('?');
+  const [uri, query] = req.url.substr(URI_PREFIX.length).split("?");
   const queryParams = {};
   // параметры могут отсутствовать вообще или иметь вид a=b&b=c
   // во втором случае наполняем объект queryParams { a: 'b', b: 'c' }
   if (query) {
-    for (const piece of query.split('&')) {
-      const [key, value] = piece.split('=');
-      queryParams[key] = value ? decodeURIComponent(value) : '';
+    for (const piece of query.split("&")) {
+      const [key, value] = piece.split("=");
+      queryParams[key] = value ? decodeURIComponent(value) : "";
     }
   }
 
   try {
     // обрабатываем запрос и формируем тело ответа
     const body = await (async () => {
-      if (uri === '' || uri === '/') {
+      if (uri === "" || uri === "/") {
         // /api/goods
-        if (req.method === 'GET') return getGoodsList(queryParams);
-
+        if (req.method === "GET") return getGoodsList(queryParams);
       } else {
         // /api/goods/{id}
         // параметр {id} из URI запроса
         const itemId = uri.substr(1);
-        if (req.method === 'GET') return getItems(itemId);
+        if (req.method === "GET") return getItems(itemId);
       }
       return null;
     })();
@@ -155,22 +150,27 @@ module.exports = server = createServer(async (req, res) => {
     } else {
       // если что-то пошло не так - пишем об этом в консоль и возвращаем 500 ошибку сервера
       res.statusCode = 500;
-      res.end(JSON.stringify({message: 'Server Error'}));
+      res.end(JSON.stringify({ message: "Server Error" }));
     }
   }
 })
   // выводим инструкцию, как только сервер запустился...
-  .on('listening', () => {
-
-    if (process.env.NODE_ENV !== 'test') {
-      console.log(`Сервер CRM запущен. Вы можете использовать его по адресу http://localhost:${PORT}`);
-      console.log('Нажмите CTRL+C, чтобы остановить сервер');
-      console.log('Доступные методы:');
+  .on("listening", () => {
+    if (process.env.NODE_ENV !== "test") {
+      console.log(
+        `Сервер CRM запущен. Вы можете использовать его по адресу http://localhost:${PORT}`
+      );
+      console.log("Нажмите CTRL+C, чтобы остановить сервер");
+      console.log("Доступные методы:");
       console.log(`GET ${URI_PREFIX} - получить список товаров`);
       console.log(`GET ${URI_PREFIX}/{id} - получить товар по его ID`);
       console.log(`GET ${URI_PREFIX}?{search=""} - найти товар по названию`);
-      console.log(`GET ${URI_PREFIX}?{category=""} - получить товар по его категории`);
-      console.log(`GET ${URI_PREFIX}?{list="{id},{id}"} - получить товары по id`);
+      console.log(
+        `GET ${URI_PREFIX}?{category=""} - получить товар по его категории`
+      );
+      console.log(
+        `GET ${URI_PREFIX}?{list="{id},{id}"} - получить товары по id`
+      );
     }
   })
   // ...и вызываем запуск сервера на указанном порту
